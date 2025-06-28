86d9c48928898d3a3c3f9842594cef004175a88b
HsunJ_Local <gkstjswo122@gmail.com>
2025-06-28 17:05:07 +0900

    update app.js, add cors

--- app.js
+++ app.js
@@ -1,72 +1,30 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
-import multer from 'multer';
 import path from 'path';
+import cors from 'cors';
 import { PrismaClient, Prisma } from '@prisma/client';
 import productRouter from './routes/product.js';
 import articleRouter from './routes/article.js';
 import uploadRouter from './routes/upload.js';
 import commentRouter from './routes/coments.js';
-import { asyncHandler } from './middlewares/asyncHandler.js';
 import { fileURLToPath } from 'url';
 
 dotenv.config();
-const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const filename = fileURLToPath(import.meta.url);
 const dir = path.dirname(filename);
 app.use(express.static(path.join(dir, 'uploads')));
 
+
 app.use('/products', productRouter);
 app.use('/articles', articleRouter);
-app.use('/files', uploadRouter);
 app.use('/comments', commentRouter )
+app.use('/files', uploadRouter);
 
-/*********** coments ***********/
-
-app.post('/articles', asyncHandler(async (req, res) => {
-  const coment = await prisma.articleComent.create({
-    data: req.body
-  })
-  res.status(201).send(coment);
-}))
-
-app.patch('/products/:id', asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  const coment = await prisma.productComent.update({
-    where: { id },
-    data: req.body
-  })
-  res.status(202).send(coment);
-}))
-
-app.patch('/articles/:id', asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  const article = await prisma.articleComent.update({
-    where: { id },
-    data: req.body
-  })
-  res.status(202).send(article);
-}))
-
-app.delete('/products/:id', asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  await prisma.productComent.delete({
-    where: { id },
-  })
-  res.sendStatus(204)
-}))
-
-app.delete('/articles/:id', asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  await prisma.articleComent.delete({
-    where: { id },
-  })
-  res.sendStatus(204)
-}))
 
 app.use('/', (err, req, res, next) => {
   if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2025') {
