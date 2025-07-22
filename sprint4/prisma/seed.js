5a91874c7c131f114fccd374c1ee78a511e7aac9
HsunJ_Local <gkstjswo122@gmail.com>
2025-07-22 18:10:09 +0900

    feat/유저, 상품, 게시물 데이터 시딩함수 작성

--- sprint4/prisma/seed.js
+++ sprint4/prisma/seed.js
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { PRODUCTS, ARTICLES, PRODUCTCOMENTS, ARTICLECOMENTS } from './mock.js';
+import { PRODUCTS, ARTICLES, PRODUCTCOMENTS, ARTICLECOMENTS, USERS } from './mock.js';
+import { hashPassword } from '../src/utils/hash.js';
 
 const prisma = new PrismaClient();
 
@@ -7,18 +8,57 @@ async function main() {
   // 기존 데이터 삭제
   await prisma.product.deleteMany();
   await prisma.article.deleteMany();
+  await prisma.user.deleteMany();
 
-  // 목 데이터 삽입
-  await prisma.product.createMany({
-    data: PRODUCTS,
-    skipDuplicates: true,
-  });
+  // 유저 데이터 삽입
+  const createdUser = {};
+  for (const userData of USERS) {
+    const { plainPassword, ...rest } = userData;
+    const { password, salt } = await hashPassword(plainPassword);
 
-  await prisma.article.createMany({
-    data: ARTICLES,
-    skipDuplicates: true
-  })
-}
+    const user = await prisma.user.create({
+      data: {
+        ...rest,
+        password,
+        salt,
+      }
+    })
+    createdUser[user.email] = user.id; // 이메일을 key, 아이디를 value로 가지는 객체 생성
+  };
+  
+  const userEmails = Object.keys(createdUser);
+  if(userEmails.length === 0){
+    console.error('생성된 유저 없음');
+    return;
+  };
+
+  // 상품 데이터 삽입
+  const createdProduct = [];
+  for(const productData of PRODUCTS){
+    const userId = getRandomUserId(createdUser, userEmails); // 유저 아이디 하나를 가져옴
+
+    createdProduct.push(await prisma.product.create({
+      data:{
+        ...productData,
+        userId,
+      }
+    }));
+  };
+
+  // 게시물 데이터 삽입
+  const createdArticle = [];
+  for(const articleData of ARTICLES){
+    const userId = getRandomUserId(createdUser, userEmails); // 유저 아이디 하나를 가져옴
+
+    createdArticle.push(await prisma.article.create({
+      data:{
+        ...articleData,
+        userId,
+      }
+    }));
+  };
+
+};
 
 main()
   .then(async () => {
@@ -29,3 +69,6 @@ main()
     await prisma.$disconnect();
     process.exit(1);
   });
+
+const getRandomUserId = (users, emails) => 
+  users[emails[Math.floor(Math.random() * emails.length)]];
