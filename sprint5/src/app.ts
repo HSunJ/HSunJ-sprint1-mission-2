b3de131ab724027806da84e4924ac692840b5ec9
HsunJ_Local <gkstjswo122@gmail.com>
2025-08-03 22:52:03 +0900

    feat/전역 에러 핸들링

--- sprint5/src/app.ts
+++ sprint5/src/app.ts
@@ -5,12 +5,15 @@ import path from 'path';
 import cors from 'cors';
 import { Prisma } from '@prisma/client';
 
+import appError from './utils/appError';
+
 import productRouter from './routes/product';
 import articleRouter from './routes/article';
 import uploadRouter from './routes/upload';
 import commentRouter from './routes/coments';
 import userRouter from './routes/users';
 import { fileURLToPath } from 'url';
+import { StructError } from 'superstruct';
 
 
 dotenv.config();
@@ -20,7 +23,7 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-const filename = fileURLToPath(import.meta.url);
+const filename = __filename;
 const dir = path.dirname(filename);
 app.use(express.static(path.join(dir, 'uploads')));
 
@@ -33,24 +36,17 @@ app.use('/users', userRouter);
 
 
 app.use('/', (err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof appError.AppError) {
+    return res.status(err.status).json({ message: err.message });
+  }
   if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2025') {
     return res.status(404).json({ message: '요청한 리소스를 찾을 수 없습니다.' });
   }
-
-  if (err.name === 'StructError' && err.failures) {
-    console.error('유효성 검사 에러:', err.failures());
+  if (err.name === 'StructError' && (err as StructError).failures) {
+    console.error('유효성 검사 에러:', (err as StructError).failures());
     return res.status(400).json({ message: '입력 형식이 올바르지 않습니다.' });
   }
-
-  if (err.code === 'credentials_required') {
-    err.code = 401;
-    err.message = "권한이 없습니다";
-  }
-  if (err.code === 'invalid_token') {
-    err.code = 401;
-    err.message = "로그인이 만료되었습니다";
-  }
-  res.status(err.code ? err.code : 500).json({ message: err.message });
+  res.status(500).json({ message: err.message });
   console.log(err.message);
 })
 
