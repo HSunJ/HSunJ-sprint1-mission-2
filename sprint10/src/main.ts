83a9fd0a04d2f4c61fdf394afab9b8f2dff01e14
HsunJ_Local <gkstjswo122@gmail.com>
2025-12-03 13:10:03 +0900

    init

--- sprint10/src/main.ts
+++
@@ -1,5 +0,0 @@
-import app from "./app";
-import { initializeSocket } from './socket/index';
-
-const { httpServer } = initializeSocket(app);
-httpServer.listen(process.env.PORT || 3000, () => console.log('Server Started'));
